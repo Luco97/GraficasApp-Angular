9f332ef73b1bf3a26b3835cd6318acb51ed31bb9
Luco <renatoluco@gmail.com>
2021-04-08 18:31:00 -0400

    complementando

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dona.component.css']
 })
 export class DonaComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
   constructor() { }
 

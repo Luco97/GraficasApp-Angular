ac7db61c9969363d9d325430f1a6c6c1fb6afd7e
Luco <renatoluco@gmail.com>
2021-04-09 15:20:54 -0400

    Creando grafico realizando peticion http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { GraficasServicesService } from '../../services/graficas-services.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = [
+    //'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+  ];
+  public doughnutChartData: MultiDataSet = [
+      //[350, 450, 100]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor( private graficasServices: GraficasServicesService) { }
 
   ngOnInit(): void {
+    this.graficasServices.getData()
+                          .subscribe(data => {
+                            const labels = Object.keys(data);
+                            const value = Object.values(data)
+
+                            this.doughnutChartLabels = labels;
+                            this.doughnutChartData.push(value);
+                          })
   }
 
 }
